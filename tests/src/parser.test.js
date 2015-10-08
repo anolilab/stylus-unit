36fd56a93853c51cda01895f3ce2fad9fb2d1b3d
anolilab <info@anolilab.de>
2015-10-08 17:09:20 +0200

    add rewire to tests internal functions

---
+++ tests/src/parser.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var rewire = require('rewire');
+var assert = require('chai').assert;
+var utils  = rewire('../specs/parser');
+
+describe('#parser:getFile', function() {
+  it('get file from path', function() {
+    assert.equal(utils.__get__('getFile')('./tests/fixture/textFile.txt'), 'dasdada');
+  });
+});
+
+describe('#parser:extractTestFromString', function() {
+  it('get file from path', function() {
+    var extractTestFromString = utils.__get__('extractTestFromString');
+    assert.equal(('./tests/fixture/textFile.txt'), 'dasdada');
+  });
+});
